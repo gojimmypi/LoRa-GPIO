0c79bd609622a138971a080bd620c253615b28d1
gojimmypi <gojimmypi@gmail.com>
2018-02-23 04:59:31 -0800

    initial working RadioHead interrupts

--- M5StackConsole/M5StackConsole.ino
+++ M5StackConsole/M5StackConsole.ino
@@ -15,13 +15,16 @@
 // #include <M5LoRa.h> // due to naming (and probably hardware!) conflicts, we cannot use both
 #include <SPI.h>
 #include <RH_RF95.h>
-#include <RadioHead.h>
+// #include <RadioHead.h>
+
+// #include <pins_arduino.h> 
 
 #include "Clock.h"
 
-#define LORA_CS_PIN   5
-#define LORA_RST_PIN  26
-#define LORA_IRQ_PIN  36
+// these defs can be found in LoRaReceiver example code for M5Stack in config.h
+//#define LORA_CS_PIN   5
+//#define LORA_RST_PIN  26
+//#define LORA_IRQ_PIN  36 //
 
 // Stock font and GFXFF reference handle
 #define GFXFF 1
@@ -40,7 +43,7 @@ Clock myClock;
 // we assume the "wing" for the ESP32 feather is connected to *different* pins as compared to M5Stack
 #define RFM95_CS 5   // LORA_CS_PIN
 #define RFM95_RST 26 // LORA_RST_PIN
-#define RFM95_INT 36 // M5 LORA_IRQ_PIN
+#define RFM95_INT 36 // M5 LORA_IRQ_PIN 36 (jumper to 16)
 
 //  ESP32 feather w/wing  see https://github.com/adafruit/RadioHead/blob/master/examples/feather/Feather9x_TX/Feather9x_TX.ino#L56
 // #define RFM95_RST     27   // "A"
@@ -49,15 +52,42 @@ Clock myClock;
 
 
 // Change to 434.0 or other frequency, must match RX's freq!
-#define RF95_FREQ 433  // RadioHead in MHz, M5 in Hz (e.g. 433E6)
+#define RF95_FREQ 433.0  // RadioHead in MHz, M5 in Hz (e.g. 433E6)
 
 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
+//static DRAM_ATTR
+int thisIndex = 0;
+int lastIndex = 0;
+//static void myISR(void *args) {
+// IRAM_ATTR 
+	 void myISR() {
+		thisIndex++;
+}
+
+void buttons_test() {
+	if (M5.BtnA.wasPressed()) {
+		M5.Lcd.printf("A");
+		Serial.printf("A");
+	}
+	if (M5.BtnB.wasPressed()) {
+		M5.Lcd.printf("B");
+		Serial.printf("B");
+	}
+	if (M5.BtnC.wasPressed()) {
+		M5.Lcd.printf("C");
+		Serial.printf("C");
+	}
+}
+ 
+
+#define OTHER 1
+#define TEST_GPIO (39) // 39 = button A; Lora Int = 36.
 void setup() {
 	// see https://www.aliexpress.com/store/product/M5Stack-Official-Stock-Offer-LoRa-Module-for-ESP32-DIY-Development-Kit-Wireless-433MHz-Built-in-Antenna/3226069_32839736315.html
-	// To aviod the problem that the screen can not display, 
+	// To avoid the problem that the screen can not display, 
 	// GPIO5, as the NSS pin of the LoRa module, needs to be pulled up when the system is initialized.
 	// M5Stack LorRa init
 	pinMode(5, OUTPUT);
@@ -68,61 +98,95 @@ void setup() {
 	Serial.begin(115200); // Serial.begin after M5.begin
 	delay(100);
 
-	Serial.println("Startup!");
+
+	int _interruptPin = TEST_GPIO;
+	int interruptNumber = digitalPinToInterrupt(_interruptPin);
+	pinMode(_interruptPin, INPUT);
+
+	int errInt = gpio_intr_enable((gpio_num_t)interruptNumber);
+	Serial.print("interruptNumber gpio_intr_enable=");
+	Serial.println(errInt);
+
+	attachInterrupt(interruptNumber, myISR, RISING);
+
+	// pinMode(35, INPUT);
+
+	//attachInterrupt(35, myISR, RISING);
+
+
+	//gpio_install_isr_service(0);
+	//gpio_isr_handler_add((gpio_num_t)TEST_GPIO, myISR, NULL);
+
+
+	// SS = 5; // RadioHead_h overwrites this TODO edit source def
+
+
+
+
+	// setup LoRa reset line
+	//pinMode(RFM95_RST, OUTPUT);
+	//digitalWrite(RFM95_RST, HIGH);
+
+
+	Serial.println("Startup! Version 1.01");
+
+#ifdef  OTHER
+
 
 	Serial.println("Pins at setup start:");
-	Serial.print("SS:        "); Serial.println(SS);
-	Serial.print("MOSI:      "); Serial.println(MOSI);
-	Serial.print("MISO:      "); Serial.println(MISO);
-	Serial.print("SCK:       "); Serial.println(SCK);
-	Serial.print("A0:        "); Serial.println(A0);
-	Serial.print("RFM95_CS:  "); Serial.println(RFM95_CS);
-	Serial.print("RFM95_RST: "); Serial.println(RFM95_RST);
-	Serial.print("RFM95_INT: "); Serial.println(RFM95_INT);
-	Serial.print("RF95_FREQ: "); Serial.println(RF95_FREQ);
+	Serial.print("  SS:        "); Serial.println(SS);
+	Serial.print("  MOSI:      "); Serial.println(MOSI);
+	Serial.print("  MISO:      "); Serial.println(MISO);
+	Serial.print("  SCK:       "); Serial.println(SCK);
+	Serial.print("  A0:        "); Serial.println(A0); // typically 36
+	Serial.print("  RFM95_CS:  "); Serial.println(RFM95_CS);
+	Serial.print("  RFM95_RST: "); Serial.println(RFM95_RST);
+	Serial.print("  RFM95_INT: "); Serial.println(RFM95_INT);
+	Serial.print("  RF95_FREQ: "); Serial.println(RF95_FREQ);
 
-
-
-
-	pinMode(RFM95_RST, OUTPUT);
-	//digitalWrite(RFM95_RST, HIGH);
 
 	Serial.println("M5Stack LoRa RX/TX Test!");
 
-	// manual reset
-	digitalWrite(RFM95_RST, LOW);
-	delay(10);
-	digitalWrite(RFM95_RST, HIGH);
-	delay(10);
+	// manual reset or LoRa module
+	//digitalWrite(RFM95_RST, LOW);
+	//delay(10);
+	//digitalWrite(RFM95_RST, HIGH);
+	//delay(10);
 
-	// manual reverse reset; desperation attempt in case reversed (does not appear that way) TODO confirm with oscilloscope
+	// manual reverse reset; desperation attempt in case reversed logic (does not appear that way) TODO confirm with oscilloscope
 	//digitalWrite(RFM95_RST, HIGH);
 	//delay(10);
 	//digitalWrite(RFM95_RST, LOW);
 	//delay(10);
 
+
+	//errInt = gpio_intr_enable((gpio_num_t) RFM95_INT);
+	//Serial.print("RFM95_INT gpio_intr_enable=");
+	//Serial.println(errInt);
+
 	// RadioHead
 	while (!rf95.init()) {
 		Serial.println("RadioHead LoRa radio init failed");
-		while (1);
+		while (1); // TODO - we don't really want to wait here forever
 	}
+	Serial.print("thisSlaveSelectPin=");
+	Serial.println(rf95.thisSlaveSelectPin());
 	Serial.println("RadioHead LoRa radio init OK!");
 
 	// Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
 	if (!rf95.setFrequency(RF95_FREQ)) {
 		Serial.println("RadioHead setFrequency failed");
-		while (1);
+		while (1); // TODO - we don't really want to wait here forever
 	}
 	Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
 
+	// do we need to match modulation settings? (or just frequency?)
 	//if (!rf95.setModemConfig(Bw125Cr48Sf4096)) { // Bw125Cr48Sf4096
 	//	Serial.println("RadioHead setModemConfig failed");
 	//	while (1);
 	//}
 	// End Radio head
 	// rf95.setModeRx(); // do we need to set Rx mode? Sample code does not: https://learn.adafruit.com/adafruit-feather-32u4-radio-with-lora-radio-module/using-the-rfm-9x-radio
-
-
 
 
 	// PA_OUTPUT_RFO_PIN = 0
@@ -144,6 +208,7 @@ void setup() {
 	// If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
 	// you can set transmitter powers from 5 to 23 dBm:
 	// rf95.setTxPower(13, false);
+	rf95.setTxPower(23, false);
 
 	// LoRa.setSyncWord(0x69);
 	Serial.println("LoRa init succeeded.");
@@ -157,8 +222,31 @@ void setup() {
 	//Serial.print("MISO:"); Serial.println(MISO);
 	//Serial.print("SCK: "); Serial.println(SCK);
 	//Serial.print("A0:  "); Serial.println(A0);
+	uint8_t data[20] = "Polling!";
+	Serial.println("Calling send...");
+
+	rf95.send(data, 20);
+	Serial.println("waiting to send...");
+	if (rf95.waitPacketSent(5000)) {
+		Serial.println("Packet send complete!"); delay(10);
+	}
+	else
+	{
+		// gave up waiting for packet to complete
+		Serial.println("Packet FAILED to complete!"); delay(10);
+	}
+	// rf95.waitPacketSent();
+	// Serial.println("Send complete!");
+
 	Serial.println("Setup Complete. Waiting for packets...");
+
+	Serial.print("rf95 mode: ");
+	Serial.println(rf95.mode());
+
+#endif //  OTHER
 }
+
+#ifdef OTHER
 
 void operationMessage(char * msg) {
 	int xpos = 0;
@@ -197,11 +285,11 @@ bool isMessageReceived() {
 int notCount = 0;
 void checkPacketReceipt() {
 	int x = 0;
-	// try to parse packet
-	M5.Lcd.setCursor(0, 160);
-	char msg[20] = "";
-	int ptr = 0;
-	char thisDevice[20];
+	//// try to parse packet
+	//M5.Lcd.setCursor(0, 160);
+	//char msg[20] = "";
+	//int ptr = 0;
+	//char thisDevice[20];
 
 	//// This is Sandeep Mistry library code
 	////int packetSize = LoRa.parsePacket();
@@ -219,7 +307,6 @@ void checkPacketReceipt() {
 	////}
 
 
-	YIELD;
 	//Serial.println(x);
 	if (rf95.available())
 	{
@@ -287,13 +374,34 @@ void checkPacketReceipt() {
 	////	//M5.Lcd.println(LoRa.packetRssi());
 	////}
 }
+#endif
 
+int lastCountInterrupt = 0;
 void loop() {
+	if (thisIndex != lastIndex) {
+		Serial.print(thisIndex);
+		Serial.println(" Interrupt!");
+		lastIndex = thisIndex;
+	}
+
+	if (rf95.countInterrupt() != lastCountInterrupt) {
+		Serial.print(rf95.countInterrupt());
+		Serial.println("RH_95 Interrupt!");
+		lastCountInterrupt = rf95.countInterrupt();
+	}
+
+	buttons_test();
+	// Serial.println(digitalRead(TEST_GPIO));
 	myClock.refreshDisplay();
 
-	buttonsProcess();
+//	buttonsProcess();
 
-	checkPacketReceipt();
+//	checkPacketReceipt();
+	yield();
+
+	YIELD;
+	delay(100);
 
 	M5.update();
+
 }
