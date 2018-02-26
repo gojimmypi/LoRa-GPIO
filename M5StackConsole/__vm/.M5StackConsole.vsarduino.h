6b6d5b32ece805cf4701c5338ce9964041361c2b
gojimmypi <gojimmypi@gmail.com>
2018-02-25 16:09:33 -0800

    working 433

--- M5StackConsole/__vm/.M5StackConsole.vsarduino.h
+++ M5StackConsole/__vm/.M5StackConsole.vsarduino.h
@@ -5,7 +5,7 @@
 			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: M5Stack-Core-ESP32, Platform=esp32, Package=esspressif
+	Hardware: M5Stack-Core-ESP32, Platform=esp32, Package=espressif
 */
 
 #if defined(_VMICRO_INTELLISENSE)
