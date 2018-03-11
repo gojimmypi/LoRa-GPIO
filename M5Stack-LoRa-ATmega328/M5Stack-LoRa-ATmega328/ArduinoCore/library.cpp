2c3c849613637fa758f2cf89b95fa629ed4114e0
gojimmypi <gojimmypi@gmail.com>
2018-03-11 12:15:26 -0700

    Added M5Stack-Lora-ATmega328 project

---
+++ M5Stack-LoRa-ATmega328/M5Stack-LoRa-ATmega328/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 3/10/2018 10:05:04 AM
+ * Author : gojimmypi
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
