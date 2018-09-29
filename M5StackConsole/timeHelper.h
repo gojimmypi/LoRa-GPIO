b98b719c9b557fd766151622c5f9ee34d6eda553
gojimmypi <gojimmypi@gmail.com>
2018-09-29 11:58:49 -0700

    add time helper

---
+++ M5StackConsole/timeHelper.h
@@ -0,0 +1,19 @@
+// timeHelper.h
+
+#ifndef _TIMEHELPER_h
+#define _TIMEHELPER_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include <sys/time.h>                   // struct timeval
+#include <time.h>
+
+void setupLocalTime();
+void printLocalTime();
+
+#endif
+
