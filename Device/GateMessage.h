d61678d2832cb904a43ba7037ee75c7512fbc744
gojimmypi <gojimmypi@gmail.com>
2018-09-30 17:48:26 -0700

    add opening & closing (not just moving)

--- Device/GateMessage.h
+++ Device/GateMessage.h
@@ -12,16 +12,20 @@
 #define GATE_LANGUAGE_EN
 
 #if defined(GATE_LANGUAGE_EN)
-#define GATE_MESSAGE_IS_OPEN    "Open"
-#define GATE_MESSAGE_IS_CLOSED  "Closed"
-#define GATE_MESSAGE_IS_MOVING  "Moving"
-#define GATE_MESSAGE_IS_ERROR   "Error"
+#define GATE_MESSAGE_IS_OPEN     "Open"
+#define GATE_MESSAGE_IS_CLOSED   "Closed"
+#define GATE_MESSAGE_IS_MOVING   "Moving"
+#define GATE_MESSAGE_IS_OPENING  "Opening"
+#define GATE_MESSAGE_IS_CLOSING  "Closing"
+#define GATE_MESSAGE_IS_ERROR    "Error"
 
 #elif defined(GATE_LANGUAGE_FR)
-#define GATE_MESSAGE_IS_OPEN    "Overt"
-#define GATE_MESSAGE_IS_CLOSED  "Ferme"
-#define GATE_MESSAGE_IS_MOVING  "En train de"
-#define GATE_MESSAGE_IS_ERROR   "Erreur"
+#define GATE_MESSAGE_IS_OPEN     "Overt"
+#define GATE_MESSAGE_IS_CLOSED   "Ferme"
+#define GATE_MESSAGE_IS_MOVING   "En marche"
+#define GATE_MESSAGE_IS_OPENING  "S'ouvre"
+#define GATE_MESSAGE_IS_CLOSING  "Se ferme"
+#define GATE_MESSAGE_IS_ERROR    "Erreur"
 
 #else
 #pragma message(Reminder "Error: GATE_LANGUAGE_xx not defined in GateMessage.h")
